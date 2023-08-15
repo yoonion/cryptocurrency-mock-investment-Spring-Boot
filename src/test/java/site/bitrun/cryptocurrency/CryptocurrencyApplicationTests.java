ca52562551d24c6b870776395b0624adb4edd6af
SANGYOUN <yoonion97@gmail.com>
2023-08-15 16:46:33 +0900

    Chore: 프로젝트 생성

---
+++ src/test/java/site/bitrun/cryptocurrency/CryptocurrencyApplicationTests.java
@@ -0,0 +1,13 @@
+package site.bitrun.cryptocurrency;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CryptocurrencyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
